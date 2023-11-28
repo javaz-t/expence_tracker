0de9eed3fff747eeb316fd1e28a0fd3b32a6d813
javaz-t <147338963+javaz-t@users.noreply.github.com>
2023-11-28 16:31:22 +0530

    firsr

---
+++ android/app/src/main/kotlin/com/example/expence_tacker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expence_tacker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(child: Text('Expence tracker')),
+        ),
+      ),
+    );
+  }
+}
